3d077cf51d3cc3dbc2cc14f8b4084dda708cba74
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2022-12-09 03:22:34 +0300

    Закончил ДЗ к 06 уроку

--- script.js
+++ script.js
@@ -3,46 +3,53 @@
 const btnStart = document.querySelector('.start')
 
 const game = (num = Math.floor(Math.random() * 100) + 1) => {
-    let count = 3
-    let start = () => {
+    let count = 10
+    console.log(num);
+    const isCount = (flag = true) => {
+        count--
+        if (count === 0) {
+            if (confirm('Попытки закончились, хотите сыграть еще?')) {
+                return game()
+            } else {
+                alert('Игра окончена')
+                return
+            }
+        }
+        flag
+            ? alert(`Загаданное число больше, осталось попыток: ${count}`)
+            : alert(`Загаданное число меньше, осталось попыток: ${count}`)
+        return start()
+    }
+    const start = () => {
         console.log(num);
-        if (count > 0) {
-            let userNum = prompt('Угадай число от 1 до 100')
-            switch (true) {
-                case userNum === null: {
+        let userNum = prompt('Угадай число от 1 до 100')
+        switch (true) {
+            case userNum === null: {
+                alert('Игра окончена')
+                return
+            }
+            case +userNum === num: {
+                if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+                    return game()
+                } else {
                     alert('Игра окончена')
                     return
                 }
-                case +userNum === num: {
-                    if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-                        return game()
-                    } else {
-                        alert('Игра окончена')
-                        return
-                    }
-                }
-                case +userNum < num: {
-                    count--
-                    alert(`Загаданное число больше, осталось попыток: ${count}`)
-                    return start()
-                }
-                case +userNum > num: {
-                    count--
-                    alert(`Загаданное число меньше, осталось попыток: ${count}`)
-                    return start()
-                }
-                default: {
-                    alert('Введи число!')
-                    return start()
-                }
+            }
+            case +userNum < num: {
+                isCount()
+                return
+            }
+            case +userNum > num: {
+                isCount(false)
+                return
+            }
+            default: {
+                alert('Введи число!')
+                return start()
             }
         }
-        if (confirm('Попытки закончились, хотите сыграть еще?')) {
-            return game()
-        } else {
-            alert('Игра окончена')
-            return
-        }
+
     }
     start()
 }
